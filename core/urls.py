cf7904471214fd6ad5d44de8bfd8a63fd6200963
Paulo Silva <paulosilva@MacBook-Air-de-Paulo.local>
2021-11-09 21:20:40 -0300

    corrigido rotas

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,6 @@ from .views import index, aluno, disciplinas
 
 urlpatterns = [
     path('', index, name='index'),
-    path('contato/', disciplinas, name='contato'),
-    path('produto/', aluno, name='produto'),
+    path('aluno/', disciplinas, name='aluno'),
+    path('disciplinas/', aluno, name='disciplinas'),
     ]
