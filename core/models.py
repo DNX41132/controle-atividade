4050068f281699765bdd4ca203dc2754f84ee404
Paulo Silva <paulosilva@MacBook-Air-de-Paulo.local>
2021-11-18 21:26:22 -0300

    criado_models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Aluno(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    atividades = models.IntegerField('Atividades', max_length=500)
+    n_entregues = models.IntegerField('Atividades não entregues', max_length=500)
+    faltas = models.IntegerField('Faltas', max_length=100)
+    descricao_atv = models.TextField('Descrição das atividades', max_length=5000)
+    evol_aluno = models.TextField('Evolução do aluno', max_length=5000)
