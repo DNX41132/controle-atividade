3d10fc7b0d6ae5b25a27a3b4d7b227fa1f18df54
Paulo Silva <paulosilva@MacBook-Air-de-Paulo.local>
2021-11-19 20:06:24 -0300

    conclusao_projeto

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Aluno
+
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'atividades', 'n_entregues', 'faltas', 'atv_pendente', 'evol_aluno')
+
+admin.site.register(Aluno, AlunoAdmin)
+admin.site.site_header = 'Controle de Atividade'
+
--- core/models.py
+++ core/models.py
@@ -4,8 +4,12 @@ from django.db import models
 
 class Aluno(models.Model):
     nome = models.CharField('Nome', max_length=100)
-    atividades = models.IntegerField('Atividades', max_length=500)
-    n_entregues = models.IntegerField('Atividades não entregues', max_length=500)
-    faltas = models.IntegerField('Faltas', max_length=100)
+    atividades = models.IntegerField('Numero de Atividades dadas')
+    n_entregues = models.IntegerField('Numero de Atividades não entregues')
+    faltas = models.IntegerField('Faltas')
     descricao_atv = models.TextField('Descrição das atividades', max_length=5000)
-    evol_aluno = models.TextField('Evolução do aluno', max_length=5000)
+    evol_aluno = models.TextField('Evolução do aluno', max_length=200)
+    atv_pendente = models.TextField('Atividades pendentes', max_length=200)
+
+    def __str__(self):
+        return self.nome
